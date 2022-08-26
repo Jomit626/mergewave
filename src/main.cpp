e8db9935cab8391f6f51baa908f86b0615c6a157
Jomit626 <jomit626@gmail.com>
2022-08-26 14:42:03 +0800

    misc: add svlang dep

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
-#include <cstdio>
 #include <iostream>
 
 #include "VCDFileParser.hpp"
+#include "slang/syntax/SyntaxTree.h"
 
 int main(int argc, char *argv[]) {
+  auto tree = slang::SyntaxTree::fromFile("asdfasdf");
   VCDFileParser parser;
-
-  std::cout << "Hello, World!";
+  VCDFile * trace = parser.parse_file("a.vcd");
+  for(VCDScope *scope: *trace->get_scopes()) {
+    for(VCDSignal *signal: scope->signals) {
+      VCDSignalValues* signalValue = trace->get_signal_values(signal->hash);
+    }
+  }
   return 0;
 }
