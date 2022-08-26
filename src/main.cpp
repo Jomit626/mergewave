064f70566e9c5250340f6974f9411ca85bed5cfc
jomit626 <jomit626@gmail.com>
2022-08-26 10:55:36 +0800

    init proj with cmake-qt and verilog-vcd-parser

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+
+#include "VCDFileParser.hpp"
+
+int main(int argc, char *argv[]) {
+  VCDFileParser parser;
+
+  std::cout << "Hello, World!";
+  return 0;
+}
